579f8f5663d8fe94be45afb2b37b29ddc81bed80
izphi78 <mtkorf@web.de>
2020-11-26 18:55:54 +0100

    Fixed setup.py read() error for Python 3.5 (#8)
    While installing pynodered in a Node-Red Docker-Container the Readme.rst file was decoded as ASCII file which lead to an error thus being not able to install the pynodered package via pip
    
    Co-authored-by: Tobias Korf <t.korf@fz-juelich.de>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.rst', encoding = 'UTF-8') as readme_file:
     readme = readme_file.read()
 
 # commented to avoid a problem with tox testing. Need a proper fix.
